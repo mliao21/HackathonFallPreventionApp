7061daec6bfab71ddeb9250829f576a6ccdc369c
mliao21 <83739501+mliao21@users.noreply.github.com>
2022-03-05 03:02:26 -0500

    added Maliha's frontend files part 1

--- FrontEnd/fall-prevent-app/src/App.js
+++ FrontEnd/fall-prevent-app/src/App.js
@@ -8,19 +8,44 @@ import {
   AddPatient,
 } from "./components";
 import BookData from "./data.json";
+import ListMedicationComponent from './components/ListMedicationComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateMedicationComponent from './components/CreateMedicationComponent';
+import ViewMedicationComponent from './components/ViewMedicationComponent';
 
 
 class App extends Component {
 
   render() {
     return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage data={BookData}/>} />
-          <Route path="/patient_profile" element={<PatientProfile />} />
-          <Route path="/new_patient" element={<AddPatient />} />
-        </Routes>
-      </Router>
+      // <Router>
+      //   <Routes>
+      //     {/* <Route path="/" element={<MainPage data={BookData} />} />
+      //     <Route path="/patient_profile" element={<PatientProfile />} />
+      //     <Route path="/new_patient" element={<AddPatient />} /> */}
+      //     <HeaderComponent />
+      // <Route path="/" exact component={ListMedicationComponent}></Route>
+      // <Route path="/meds" component={ListMedicationComponent}></Route>
+      // <Route path="/add-med/:id" component={CreateMedicationComponent}></Route>
+      // <Route path="/view-med/:id" component={ViewMedicationComponent}></Route>
+      //     <FooterComponent />
+      //   </Routes>
+      // </Router>
+      <div>
+        <Router>
+          <HeaderComponent />
+          <div className="container">
+            <Routes>
+              <Route path="/" exact component={ListMedicationComponent}></Route>
+              <Route path="/meds" component={ListMedicationComponent}></Route>
+              <Route path="/add-med/:id" component={CreateMedicationComponent}></Route>
+              <Route path="/view-med/:id" component={ViewMedicationComponent}></Route>
+            </Routes>
+          </div>
+          <FooterComponent />
+        </Router>
+      </div>
     );
   }
 }
