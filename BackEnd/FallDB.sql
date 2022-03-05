ecb95600cf03984f909c8993168268d44d9aca9b
mliao21 <83739501+mliao21@users.noreply.github.com>
2022-03-05 12:47:08 -0500

    no changes

--- BackEnd/FallDB.sql
+++ BackEnd/FallDB.sql
@@ -25,17 +25,17 @@ VALUES
 ('Beatrice Thatcher', 'O+', 'F', '24294589', 'Chronic Kidney Failure, MS, IBS', '141.09568','1939-1-5'),
 ('Victor Jenkins', 'B+', 'M', '17392815', 'COPD, Hypothyroidism, Parkinsons Disease', '138.89106','1951-2-5'),
 ('Silas Hope', 'AB+', 'M', '77884403', 'Glaucoma, Incontinence, Urinary, Staph Infections', '130.07258','1960-2-5'),
-('Emily West', 'A-', 'F', '75202717', 'Cholesterol, Hyperthyroidism, Wrinkles', '145.50492','1942-11-12'),
-('Arthur Langley', 'B-', 'M', '10701730', 'Atrial Fibrillation, Osteoarthritis, Periodontitis', '160.93726','1949-8-8'),
-('Dorothy Hansley', 'B+', 'F', '35842333', 'Alzheimers Disease, Chronic Kidney Failure, Thrush, Oral', '156.52802','1955-9-1'),
-('Amelia Solace', 'AB-', 'F', '32176747', 'HIV/AIDS, Chronic Kidney Failure, TB', '158.73264','1960-1-1'),
-('Ava Levine', 'O+', 'F', '27625301', 'Hypertension, Liver Failure, Acute, TIA', '127.86796','1950-12-12'),
-('Edith Monroe', 'A-', 'F', '64887564', 'Cancer, Melanoma, Skin Cancer, Vascular Dementia', '121.2541','1952-2-12'),
-('Cordelia Bardot', 'B-', 'F', '83683626', 'Bedsores, IBD, Diabetes', '149.91416','1953-10-12'),
-('Cyrus Daughtler', 'O-', 'M', '91942731', 'Heart Disease, Parkinsons Disease, Vertigo', '149.91416','1938-8-11'),
-('Harvey Lopez', 'A-', 'M', '98836361', 'Bipolar Disorder, Non-Hodgkins Lymphoma, Ulcerative Colitis', '108.02638','1948-9-1'),
-('Bessie Raven', 'O+', 'F', '43476740', 'Diabetes, Liver Spots, Bipolar Disorder', '156.52802','1951-8-1'),
-('Harriet Cromwell', 'AB-', 'F', '73894787', 'Autism, Hodgkins disease, Stroke', '121.2541','1970-1-3');
+('Emily West', 'A-', 'F', '75202717', 'Cholesterol, Hyperthyroidism, Wrinkles', '145.50492','1942-11-12');
+-- ('Arthur Langley', 'B-', 'M', '10701730', 'Atrial Fibrillation, Osteoarthritis, Periodontitis', '160.93726','1949-8-8'),
+-- ('Dorothy Hansley', 'B+', 'F', '35842333', 'Alzheimers Disease, Chronic Kidney Failure, Thrush, Oral', '156.52802','1955-9-1'),
+-- ('Amelia Solace', 'AB-', 'F', '32176747', 'HIV/AIDS, Chronic Kidney Failure, TB', '158.73264','1960-1-1'),
+-- ('Ava Levine', 'O+', 'F', '27625301', 'Hypertension, Liver Failure, Acute, TIA', '127.86796','1950-12-12'),
+-- ('Edith Monroe', 'A-', 'F', '64887564', 'Cancer, Melanoma, Skin Cancer, Vascular Dementia', '121.2541','1952-2-12'),
+-- ('Cordelia Bardot', 'B-', 'F', '83683626', 'Bedsores, IBD, Diabetes', '149.91416','1953-10-12'),
+-- ('Cyrus Daughtler', 'O-', 'M', '91942731', 'Heart Disease, Parkinsons Disease, Vertigo', '149.91416','1938-8-11'),
+-- ('Harvey Lopez', 'A-', 'M', '98836361', 'Bipolar Disorder, Non-Hodgkins Lymphoma, Ulcerative Colitis', '108.02638','1948-9-1'),
+-- ('Bessie Raven', 'O+', 'F', '43476740', 'Diabetes, Liver Spots, Bipolar Disorder', '156.52802','1951-8-1'),
+-- ('Harriet Cromwell', 'AB-', 'F', '73894787', 'Autism, Hodgkins disease, Stroke', '121.2541','1970-1-3');
 
 
 DROP TABLE IF EXISTS patientdata;
@@ -58,9 +58,30 @@ VALUES
 (1,"2021-1-1","11pm","3",36.6,"120/80",70,"95%","good"),
 (1,"2021-1-2","12pm","3",36.5,"120/85",74,"92%","good"),
 (1,"2021-1-3","10pm","3",36.7,"125/89",80,"91%","good"),
-(2,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
-(2,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
-(2,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good");
+(3,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(3,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(3,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(4,"2021-1-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(4,"2021-1-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(4,"2021-1-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(5,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(5,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(5,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(6,"2021-1-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(6,"2021-1-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(6,"2021-1-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(7,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(7,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(7,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(8,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(8,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(8,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(9,"2021-1-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(9,"2021-1-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(9,"2021-1-3","10pm","3",36.7,"125/89",80,"91%","good"),
+(10,"2021-2-1","11pm","3",36.6,"120/80",70,"95%","good"),
+(10,"2021-2-2","12pm","3",36.5,"120/85",74,"92%","good"),
+(10,"2021-2-3","10pm","3",36.7,"125/89",80,"91%","good");
 
 DROP TABLE IF EXISTS comment;
 CREATE TABLE comment(
@@ -74,11 +95,23 @@ CREATE TABLE comment(
 );
 INSERT INTO comment (patientid, comment,date)
 VALUES 
-(1, "commemnt for patient 1","2021-10-10"),
-(1, "another commemnt for patient 1","2021-11-20"),
-(2, "commemnt for patient 2","2021-11-11"),
-(2, "another commemnt for patient 2","2022-1-2");
-
+(1, "Walker should be within reach at all times","2021-10-10"),
+(1, "Daily strengthening exercise after breakfast","2021-11-20"),
+(2, "Give Tylenol before getting patient up in AM","2021-11-11"),
+(2, "Update family if you notice changes in the patient's condition","2022-1-2"),
+(3, "Ask if there is fear of falling or reduction in usual activities","2022-1-3"),
+(3, "Note concerns with feeding and swallowing","2022-1-4"),
+(4, "Note signs and symptomps of depression and aggitation or anxiety","2022-1-5"),
+(4, "Show patients how to use their assistive device when getting up","2022-1-6"),
+(5, "Ask about light-headedness or dizziness","2022-1-7"),
+(5, "Be mindful of anti-psychotic and anti-depressants","2022-1-8"),
+(6, "Note use of alchohol and street recreational drugs","2022-1-9"),
+(6, "Assistance required fo daily activities e.g bathing, dressing","2022-1-10"),
+(7, "Keep bed breaks locked","2022-1-11"),
+(7, "Note presence of shortness of breath at rest or with minimal activity","2022-1-12"),
+(8, "Note signs and symptomps of depression and aggitation or anxiety","2022-1-5"),
+(9, "Show patients how to use their assistive device when getting up","2022-1-6"),
+(10, "Note use of alchohol and street recreational drugs","2022-1-9");
 
 DROP TABLE IF EXISTS falldata;
 CREATE TABLE falldata( 
@@ -92,11 +125,30 @@ CREATE TABLE falldata(
 );
 INSERT into falldata (patientid, frop_score, fropdate) 
 VALUES
-(1,8,"2021-1-2"),
+(1,5,"2021-1-2"),
 (1,7,"2021-5-1"),
 (1,7,"2021-5-1"),
 (2,2,"2021-5-2"),
-(2,2,"2021-6-10");
+(2,2,"2021-6-10"),
+(3,3,"2021-1-2"),
+(3,5,"2021-5-1"),
+(3,7,"2021-5-1"),
+(4,3,"2021-5-2"),
+(4,5,"2021-6-10"),
+(5,2,"2021-1-2"),
+(5,2,"2021-5-1"),
+(5,3,"2021-5-1"),
+(6,5,"2021-5-2"),
+(6,8,"2021-6-10"),
+(7,2,"2021-5-2"),
+(7,4,"2021-6-10"),
+(8,1,"2021-1-2"),
+(8,2,"2021-5-1"),
+(8,3,"2021-5-1"),
+(9,3,"2021-5-2"),
+(9,2,"2021-6-10"),
+(10,8,"2021-5-2"),
+(10,10,"2021-6-10");
 
 -- INSERT INTO recs(frop_score, recommendation, risk_factor)
 -- VALUES
